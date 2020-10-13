23535c973ffcd93d4716fe0b75f25d2c799e9fec
Tim Chisamore <tim.chisamore@gmail.com>
2020-10-13 09:31:24 -0400

    Adjusting to push unknown and pending outcomes into the recovered classes as outlined.

--- R/generate_recovered_ids.R
+++ R/generate_recovered_ids.R
@@ -17,18 +17,28 @@ generate_recovered_ids <- function(clean_ccm_investigations_data, clean_ccm_outc
   investigation_numbers <- clean_ccm_investigations_data %>%
     pull(investigation_number)
 
-  # extracing the investigation numbers of any cases who have recovered
+  # extracing the investigation numbers of any cases who have an outcome of recovered
   recovered_ids <- clean_ccm_outcomes_data %>%
     filter(fct_match(outcome, "RECOVERED")) %>%
     pull(investigation_number)
 
-  # extracting the investigation numbers of any cases who have residual effects
+  # extracting the investigation numbers of any cases who have an outcome of residual effects
   residual_effects_ids <- clean_ccm_outcomes_data %>%
     filter(fct_match(outcome, "RES. EFFECTS")) %>%
     pull(investigation_number)
+  
+  # extracting the investigation numbers of any cases who have an outcome of pending
+  pending_effects_ids <- clean_ccm_outcomes_data %>%
+    filter(fct_match(outcome, "PENDING")) %>%
+    pull(investigation_number)
+  
+  # extracting the investigation numbers of any cases who have an outcome of unknown
+  unknown_ids <- clean_ccm_outcomes_data %>%
+    filter(fct_match(outcome, "UNKNOWN")) %>%
+    pull(investigation_number)
 
   # taking the union of recovered and residual efects IDs
-  recovered_ids <- union(recovered_ids, residual_effects_ids)
+  recovered_ids <- union(recovered_ids, union(residual_effects_ids, union(pending_effects_ids, unknown_ids)))
 
   # ensuring all of our generated IDs are valid
   assert_generated_ids(investigation_numbers, recovered_ids)
