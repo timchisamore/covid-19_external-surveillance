244f69ad9e99fc817c008d82001597ccf36d3250
Tim Chisamore <tim.chisamore@gmail.com>
2020-12-18 11:03:00 -0500

    Using lubridate to parse datetime fields properly instead of just chopping off the time component and treating it as a date. Note, we did have to remove the periods from the AM or PM indicator.

--- R/cleaning_ccm_investigations_data.R
+++ R/cleaning_ccm_investigations_data.R
@@ -16,8 +16,10 @@ cleaning_ccm_investigations_data <- function(raw_ccm_investigations_data) {
     janitor::remove_empty(which = "cols") %>%
     janitor::clean_names() %>%
     mutate(
-      across(.cols = contains("date"), .fns = str_extract, pattern = "^[0-9]{4}-[0-9]{2}-[0-9]{2}"),
-      across(.cols = contains("date"), .fns = lubridate::ymd)
+      across(.cols = contains("date"), .fns = str_remove_all, pattern = "\\."),
+      across(.cols = contains("date"), .fns = lubridate::parse_date_time, orders = c("%Y-%m-%d, %H:%M %p", "%Y-%m-%d")),
+      permanent_city_at_illness = str_to_upper(permanent_city_at_illness),
+      current_city_at_illness = str_to_upper(current_city_at_illness)
     )
 
   return(clean_ccm_investigations_data)
--- R/cleaning_ccm_outcomes_data.R
+++ R/cleaning_ccm_outcomes_data.R
@@ -15,8 +15,8 @@ cleaning_ccm_outcomes_data <- function(raw_ccm_outcomes_data) {
     janitor::remove_empty(which = "cols") %>%
     janitor::clean_names() %>%
     mutate(
-      across(.cols = contains("date"), .fns = str_extract, pattern = "^[0-9]{4}-[0-9]{2}-[0-9]{2}"),
-      across(.cols = contains("date"), .fns = lubridate::ymd)
+      across(.cols = contains("date"), .fns = str_remove_all, pattern = "\\."),
+      across(.cols = contains("date"), .fns = lubridate::parse_date_time, orders = c("%Y-%m-%d, %H:%M %p", "%Y-%m-%d"))
     )
 
   return(clean_ccm_outcomes_data)
